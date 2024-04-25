def52420fb97fd0d2e517de33c3781fa1cd89a9b
LucaKeiser <luca.keiser@hotmail.com>
2024-04-25 23:04:47 +0200

    update weigth-skript

--- gfs.bern/02_scripts/weigth_function_TEST.R
+++ gfs.bern/02_scripts/weigth_function_TEST.R
@@ -14,18 +14,12 @@ source(here::here("gfs.bern",
 
 
 
-# Daten laden -------------------------------------------------------------
-df <- haven::read_sav(here::here("gfs.bern",
-                                 "01_data",
-                                 "ESS_subset.sav"))
-
-
-
-
-
 # Beispiel reproduzieren --------------------------------------------------
+
+## Daten laden ------------------------------------------------------------
 data("anes04")
 
+# aufbereiten
 anes04 <- anes04 %>% 
   rowid_to_column() %>% 
   mutate(age_grouped = case_when(
@@ -34,27 +28,60 @@ anes04 <- anes04 %>%
     age > 50 ~ 3)
   )
 
+
+## Soll-Werte definieren (aka Targets) ------------------------------------
 agetarg <- c(0.2, 0.5, 0.3)
 names(agetarg) <- c(1, 2, 3)
 marriedtarget <- c(0.4, 0.6)
 names(marriedtarget) <- c(0, 1)
 
 targets <- list(agetarg, marriedtarget)
-
 names(targets) <- c("age_grouped", "married")
 
-outsave <- anesrake(inputter = targets, 
+
+## Gewichtung berechnen ---------------------------------------------------
+weigths <- anesrake(inputter = targets, 
                     dataframe = anes04, 
                     caseid = anes04$rowid,
                     verbose = TRUE)
 
+# mit Daten mergen
 weigths <- tibble(
-  idno = outsave$caseid,
-  weigth = outsave$weightvec
+  rowid = weigths$caseid,
+  gewdef = weigths$weightvec
 )
 
+anes04 <- anes04 %>% 
+  arrange(rowid) %>% 
+  bind_cols(weigths %>% 
+              arrange(rowid) %>% 
+              select(gewdef)) %>% 
+  as_tibble()
 
 
+## Check ------------------------------------------------------------------
+# age_grouped
+anes04 %>% 
+  count(age_grouped,
+        wt = gewdef) %>% 
+  mutate(pct = n / sum(n))
+
+# married
+anes04 %>% 
+  count(married,
+        wt = gewdef) %>% 
+  mutate(pct = n / sum(n))
+
+
+
+
+
+# Eigenes Beispiel --------------------------------------------------------
+
+## Daten laden ------------------------------------------------------------
+df <- haven::read_sav(here::here("gfs.bern",
+                                 "01_data",
+                                 "ESS_subset.sav"))
 
 ## Beispieldatensatz erstellen --------------------------------------------
 df <- df %>% 
@@ -77,86 +104,68 @@ df <- df %>%
         between(lrscale, 3, 6) ~ 2,
         lrscale > 6 ~ 3
       )
-  ) %>% 
-  mutate(age_group = 
-           labelled_spss(age_group,
-                         label =  "Alter gruppiert",
-                         labels = c("-35" = 1,
-                                    "36-45" = 2,
-                                    "46-65" = 3,
-                                    "65+" = 4)
-           )
-  ) %>% 
-  mutate(lrscaleg = 
-           labelled_spss(lrscaleg,
-                         label =  "Links-Rechts-Skala gruppiert",
-                         labels = c("links" = 1,
-                                    "mitte" = 2,
-                                    "rechts" = 3)
-           )
   )
 
 
-
-
-
-# Gewichtung erstellen ----------------------------------------------------
-
-# Ist-Wert
-df %>% 
-  count(gndr) %>% 
-  mutate(pct = n / sum(n))
-
-df %>% 
-  count(age_group) %>% 
-  mutate(pct = n / sum(n))
-
-df %>% 
-  count(lrscaleg) %>% 
-  mutate(pct = n / sum(n))
-
-
-# Soll-Wert
+## Soll-Werte definieren (aka Targets) ------------------------------------
 gndr <- c(0.6, 0.4)
+names(gndr) <- c(1, 2)
+
 age_group <- c(0.3, 0.2, 0.35, 0.15)
+names(age_group) <- c(1, 2, 3, 4)
+
 lrscaleg <- c(0.25, 0.5, 0.25)
+names(lrscaleg) <- c(1, 2, 3)
 
-targets <- list(gndr,
-                age_group,
-                lrscaleg)
-
+targets <- list(gndr, age_group, lrscaleg)
 names(targets) <- c("gndr",
                     "age_group",
                     "lrscaleg")
 
-# Check
-map(targets, sum)
 
+## Gewichtung berechnen ---------------------------------------------------
+weigths <- anesrake(inputter = targets,  
+                    dataframe = df,
+                    caseid = df$idno,
+                    verbose = TRUE)
 
-
-df_weigthed <- df %>% 
-  filter(!is.na(gndr)) %>% 
-  filter(!is.na(age_group)) %>% 
-  filter(!is.na(lrscale)) %>% 
-  zap_labels() %>% 
-  zap_label()
-
-my_weigths <- anesrake(
-  inputter = targets,
-  dataframe = df_weigthed,
-  caseid = df_weigthed$idno
+# mit Daten mergen
+weigths <- tibble(
+  idno = weigths$caseid,
+  gewdef = weigths$weightvec
 )
 
+df_gewdef <- df %>% 
+  left_join(weigths,
+            by = "idno") %>% 
+  # NAs werden auf 1 gesetzt
+  mutate(gewdef = ifelse(is.na(gewdef),
+                         1, 
+                         gewdef))
 
 
 
+## Check ------------------------------------------------------------------
+df_gewdef %>% 
+  count(gndr,
+        wt = gewdef) %>% 
+  mutate(pct_data = n / sum(n),
+         pct_target = targets$gndr,
+         difference = pct_target - pct_data)
 
+df_gewdef %>% 
+  filter(!is.na(age_group)) %>% 
+  count(age_group,
+        wt = gewdef) %>% 
+  mutate(pct_data = n / sum(n),
+         pct_target = targets$age_group,
+         difference = pct_target - pct_data)
 
-
-
-
-
-
-
-
+df_gewdef %>% 
+  filter(!is.na(lrscaleg)) %>%
+  count(lrscaleg,
+        wt = gewdef) %>%
+  mutate(pct_data = n / sum(n),
+         pct_target = targets$lrscale,
+         difference = pct_target - pct_data)
 
